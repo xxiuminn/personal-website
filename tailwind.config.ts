19c1246de49e0fb0c72a66ff6fabbedf341efa60
xxiuminn <xiumin1997@gmail.com>
2024-08-10 19:50:34 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,9 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      opensans: ["Open Sans"],
+    },
     extend: {},
   },
   plugins: [],
