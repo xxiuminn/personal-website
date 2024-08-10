032575372a366edb9a3d347f3c73136eb784c865
xxiuminn <xiumin1997@gmail.com>
2024-08-10 21:16:16 +0800

    update with placeholder

---
+++ src/components/Project.tsx
@@ -0,0 +1,40 @@
+const Project = () => {
+  return (
+    <>
+      <div className="rounded-xl bg-white">
+        <img></img>
+        <h3 className="text-2xl">title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+          suscipit, temporibus eveniet cumque cum iure necessitatibus ut
+          distinctio quas. Repellat maxime fugit sint blanditiis possimus, iusto
+          illum praesentium cupiditate consectetur.
+        </p>
+      </div>
+
+      <div className="rounded-xl bg-white">
+        <img></img>
+        <h3 className="text-2xl">title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+          suscipit, temporibus eveniet cumque cum iure necessitatibus ut
+          distinctio quas. Repellat maxime fugit sint blanditiis possimus, iusto
+          illum praesentium cupiditate consectetur.
+        </p>
+      </div>
+
+      <div className="rounded-xl bg-white">
+        <img></img>
+        <h3 className="text-2xl">title</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
+          suscipit, temporibus eveniet cumque cum iure necessitatibus ut
+          distinctio quas. Repellat maxime fugit sint blanditiis possimus, iusto
+          illum praesentium cupiditate consectetur.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Project;
