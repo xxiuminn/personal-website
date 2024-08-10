c04bb3f39b91ae818130ab616a68d51bdfc5dab2
xxiuminn <xiumin1997@gmail.com>
2024-08-10 19:50:26 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Profile from "./components/Profile";
+import Projects from "./components/Projects";
+
 function App() {
-  return <>hi</>;
+  return (
+    <div className="">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        <Profile />
+        <Projects />
+      </div>
+    </div>
+  );
 }
 
 export default App;
