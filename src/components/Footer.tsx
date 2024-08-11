193eb2f29c7cd13c73ea59ae527948b59fb46dbf
xxiuminn <xiumin1997@gmail.com>
2024-08-11 23:25:56 +0800

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bottom-0 text-center font-worksans font-light py-10">
+      Built using React & Tailwind.
+    </footer>
+  );
+};
+
+export default Footer;
