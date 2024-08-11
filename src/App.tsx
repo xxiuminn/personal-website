c781dc00b586190b859dc4a7cc719673184a0d66
xxiuminn <xiumin1997@gmail.com>
2024-08-11 23:26:05 +0800

    update css grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import Footer from "./components/Footer";
 import Profile from "./components/Profile";
 import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 p-10 md:px-24 gap-4">
-      <Profile />
-      <Projects />
-    </div>
+    <>
+      <div className="grid grid-cols-1 lg:grid-cols-2 md:px-24 px-10 lg:gap-20 gap-10">
+        <Profile />
+        <Projects />
+      </div>
+      <Footer />
+    </>
   );
 }
 
