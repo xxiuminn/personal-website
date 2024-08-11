c67608b1b05a71aead9919a42d197cfc15cbecdc
xxiuminn <xiumin1997@gmail.com>
2024-08-11 23:36:11 +0800

    add icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
   return (
     <footer className="bottom-0 text-center font-worksans font-light py-10">
-      Built using React & Tailwind.
+      Built using React <span className="i-logos-react align-middle"></span> &
+      Tailwind <span className="i-devicon-tailwindcss align-middle"></span>
     </footer>
   );
 };
